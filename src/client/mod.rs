9fc33ca389864749e763f98a4063864a7149dd75
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-04-06 20:03:59 +0200

    Add ability to fetch the Nextcloud server version
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,19 +1,45 @@
+extern crate hyper;
+extern crate json;
+
+use std::io::Read;
+
 pub trait Client {
-    fn test(&self);
+    fn status(&self) -> Result<i32, &'static str>;
 }
 
 pub struct ClientImpl<'a> {
-    url: &'a str
+    url: &'a str,
+    http_client: hyper::client::Client,
+}
+
+fn parse_status(text: &mut String) -> Result<i32, &'static str> {
+    match json::parse(&text) {
+        Ok(status) => {
+            println!("Nextcloud version: {}", status["version"]);
+            return Ok(0);
+        }
+        Err(_) => {
+            return Err("error while parsing the resonse");
+        }
+    }
 }
 
 impl<'a> Client for ClientImpl<'a> {
-    fn test (&self) {
-        println!("{}", self.url);
+    fn status(&self) -> Result<i32, &'static str> {
+        let mut url = self.url.to_string();
+        url.push_str("/status.php");
+        let mut res = self.http_client.get(&url).send().unwrap();
+
+        let mut resp_text = "".to_string();
+        match res.read_to_string(&mut resp_text) {
+            Ok(_) => parse_status(&mut resp_text),
+            Err(_) => Err("error while reading response body"),
+        }
     }
 }
 
 pub struct Builder<'a> {
-    url: &'a str  
+    url: &'a str,
 }
 
 impl<'a> Builder<'a> {
@@ -21,7 +47,11 @@ impl<'a> Builder<'a> {
         Builder { url: url }
     }
 
+    // TODO: return Client trait object and hide impl
     pub fn finalize(&self) -> ClientImpl {
-        ClientImpl { url: self.url}
+        ClientImpl {
+            url: self.url,
+            http_client: hyper::client::Client::new(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use nextcloud::client::Client;
 use nextcloud::client;
 
 fn main() {
-    let url = "test";
+    let url = "http://localhost:8080";
     let builder = client::Builder::new(url);
     let client = builder.finalize();
 
-    client.test();
-
-    print!("hello");
+    match client.status() {
+        Ok(_) => {
+            println!("could load status");
+        }
+        Err(error_message) => {
+            println!("could not load status: {}", error_message);
+        }
+    }
 }
